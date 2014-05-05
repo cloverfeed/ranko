8d88b3e8e437c468b8b7e15eb5e936e9e45277e7
Etienne Millon <me@emillon.org>
2014-05-05 20:05:52 +0200

    upload

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from flask.ext.wtf import Form
+from flask_wtf.file import FileField
+import os
+from key import get_secret_key
 
 app = Flask('Review')
+key_file = os.path.join(app.instance_path, 'secret.key')
+app.config['SECRET_KEY'] = get_secret_key(key_file)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+class UploadForm(Form):
+    file = FileField('The file to review')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    form = UploadForm()
+    if form.validate_on_submit():
+        flash('Uploaded')
+        return redirect(url_for('home'))
+    return render_template('upload.html', form=form)
 
 def main():
     pass
---
+++ key.py
@@ -0,0 +1,17 @@
+from random import choice
+from string import ascii_uppercase, digits
+
+
+def generate_key(n):
+    return ''.join(choice(ascii_uppercase + digits) for x in range(n))
+
+
+def get_secret_key(filename):
+    try:
+        with open(filename) as f:
+            secret = f.read()
+    except IOError as e:
+        secret = generate_key(64)
+        with open(filename, 'w+') as f:
+            f.write(secret)
+    return secret
