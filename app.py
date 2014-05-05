37913ba38dd8b6da7545bc70fec455d8f12761d5
Etienne Millon <me@emillon.org>
2014-05-05 20:45:35 +0200

    uploads

--- app.py
+++ app.py
@@ -3,10 +3,36 @@ from flask.ext.wtf import Form
 from flask_wtf.file import FileField
 import os
 from key import get_secret_key
+from flask.ext.uploads import UploadSet
+from flask.ext.uploads import configure_uploads
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask('Review')
+
+# CSRF & WTForms
 key_file = os.path.join(app.instance_path, 'secret.key')
 app.config['SECRET_KEY'] = get_secret_key(key_file)
+
+# flask-uploads
+documents = UploadSet('documents', extensions=['pdf'],
+        default_dest=lambda app: os.path.join(app.instance_path, 'uploads'))
+configure_uploads(app, [documents])
+
+# flask-sqlalchemy
+uri = 'sqlite:///' + os.path.join(app.instance_path, 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+db = SQLAlchemy(app)
+
+class Document(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    filename = db.Column(db.String, nullable=False)
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    @staticmethod
+    def get_by_id(id):
+        return db.session.query(Document).get(id)
 
 
 @app.route('/')
@@ -20,9 +46,18 @@ class UploadForm(Form):
 def upload():
     form = UploadForm()
     if form.validate_on_submit():
+        filename = documents.save(form.file.data)
+        doc = Document(filename)
+        db.session.add(doc)
+        db.session.commit()
         flash('Uploaded')
-        return redirect(url_for('home'))
+        return redirect(url_for('view_doc', id=doc.id))
     return render_template('upload.html', form=form)
+
+@app.route('/view/<id>')
+def view_doc(id):
+    doc = Document.get_by_id(id)
+    return render_template('view.html', doc=doc)
 
 def main():
     pass
---
+++ makedb.py
@@ -0,0 +1,7 @@
+from app import db
+
+def main():
+    db.create_all()
+
+if __name__ == '__main__':
+    main()
