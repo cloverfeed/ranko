3c3a69cba7b26ba5b38a64f69bcd4ebb2a90a69b
Etienne Millon <me@emillon.org>
2014-05-06 23:13:46 +0200

    comments

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, flash, redirect, url_for, send_from_directory
 from flask.ext.wtf import Form
 from flask_wtf.file import FileField
+from wtforms import TextAreaField, HiddenField
 import os
 from key import get_secret_key
 from flask.ext.uploads import UploadSet
@@ -54,10 +55,40 @@ def upload():
         return redirect(url_for('view_doc', id=doc.id))
     return render_template('upload.html', form=form)
 
+class CommentForm(Form):
+    docid = HiddenField('Document ID')
+    comment = TextAreaField('Comment')
+
 @app.route('/view/<id>')
 def view_doc(id):
     doc = Document.get_by_id(id)
-    return render_template('view.html', doc=doc)
+    form = CommentForm(docid=id)
+    comments = Comment.all_for_doc(id)
+    return render_template('view.html', doc=doc, form=form, comments=comments)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    doc = db.Column(db.Integer, db.ForeignKey('comment.id'), nullable=False)
+    text = db.Column(db.String, nullable=False)
+
+    def __init__(self, doc, text):
+        self.doc = doc
+        self.text = text
+
+    @staticmethod
+    def all_for_doc(doc):
+        return db.session.query(Comment).filter_by(doc=doc)
+
+@app.route('/comment/new', methods=['POST'])
+def post_comment():
+    form = CommentForm()
+    assert(form.validate_on_submit())
+    docid = form.docid.data
+    comm = Comment(docid, form.comment.data)
+    db.session.add(comm)
+    db.session.commit()
+    flash("Comment saved")
+    return redirect(url_for('view_doc', id=form.docid.data))
 
 @app.route('/raw/<id>')
 def rawdoc(id):
