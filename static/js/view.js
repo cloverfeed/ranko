4ca21d8c4d59c5b80178028f8a150e3b0935b762
Etienne Millon <me@emillon.org>
2014-05-12 16:34:59 +0200

    load page per page

--- static/js/view.js
+++ static/js/view.js
@@ -1,14 +1,22 @@
 function view_pdf(pv, pdf) {
-    for(var i = 1; i <= pdf.numPages ; i ++){
-        pdf.getPage(i).then(function(page){
-            var canvas = document.createElement('canvas');
-            var context = canvas.getContext('2d');
-            var scale = 1.2;
-            var viewport = page.getViewport(scale);
-            canvas.width = viewport.width;
-            canvas.height = viewport.height;
-            page.render({canvasContext: context, viewport: viewport});
-            pv.appendChild(canvas);
+    pdf.getPage(1).then(function(page) {
+        render_page(pv, pdf, 1, page);
+    });
+}
+
+function render_page(pv, pdf, i, page) {
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    var scale = 1.2;
+    var viewport = page.getViewport(scale);
+    canvas.width = viewport.width;
+    canvas.height = viewport.height;
+    page.render({canvasContext: context, viewport: viewport});
+    pv.appendChild(canvas);
+
+    if (i+1 <= pdf.numPages) {
+        pdf.getPage(i+1).then(function(page){
+            render_page(pv, pdf, i+1, page);
         });
     }
 }
