b8703be615b4387fa8093581608c8ff4c03aca25
Etienne Millon <me@emillon.org>
2014-10-13 17:47:10 +0200

    Make text selectable
    Closes #7

--- static/js/view.js
+++ static/js/view.js
@@ -11,8 +11,35 @@ function render_page(pv, pdf, i, page) {
     var viewport = page.getViewport(scale);
     canvas.width = viewport.width;
     canvas.height = viewport.height;
-    page.render({canvasContext: context, viewport: viewport});
     pv.appendChild(canvas);
+    var renderContext = {
+        canvasContext: context,
+        viewport: viewport,
+    };
+    page.render(renderContext);
+    var $textLayerDiv = jQuery("<div />")
+        .addClass("textLayer")
+        .css("height", viewport.height + "px")
+        .css("width", viewport.width + "px")
+        .offset({
+            top: canvas.offsetTop,
+            left: canvas.offsetLeft
+        });
+    pv.appendChild($textLayerDiv.get(0));
+    page.getTextContent().then(function(textContent) {
+        var pageNumber = 1;
+        var pageIndex = pageNumber - 1;
+        var tlbOptions = {
+            textLayerDiv: $textLayerDiv.get(0),
+            pageIndex: pageIndex,
+            viewport: viewport,
+        };
+        var textLayer = new TextLayerBuilder(tlbOptions);
+        textLayer.setTextContent(textContent);
+    }).then(null, function(error){
+        pv.innerHTML = "Error while rendering PDF.";
+        console.log("Error while rendering PDF: " + error);
+    });
 
     if (i+1 <= pdf.numPages) {
         pdf.getPage(i+1).then(function(page){
