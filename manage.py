0d1a5e0e09efc2435440ae8577295e610f2b7c02
Etienne Millon <me@emillon.org>
2014-10-27 17:34:26 +0100

    merge makedb.py into manage.py

--- manage.py
+++ manage.py
@@ -1,14 +1,21 @@
 """
 Flask-script stuff
 """
-from flask.ext.migrate import MigrateCommand
+from flask.ext.migrate import MigrateCommand, stamp
 from flask.ext.script import Manager
 from app.factory import create_app
+from app.models import db
 
 def main():
     manager = Manager(create_app)
     manager.add_command('db', MigrateCommand)
     manager.add_option('-c', '--config', dest='config_file', required=False)
+
+    @manager.command
+    def create():
+        "Creates database tables from sqlalchemy models"
+        db.create_all()
+        stamp()
 
     manager.run()
 
