14bc5367fcdad5c7683360d88daac1f6f618c39a
Etienne Millon <me@emillon.org>
2014-10-29 14:44:54 +0100

    use flask-testing methods
    Closes #25

--- app/tests.py
+++ app/tests.py
@@ -31,33 +31,33 @@ class TestCase(TestCase):
 
     def test_upload(self):
         r = self._upload('toto.pdf')
-        self.assertEqual(r.status_code, 302)
+        self.assertStatus(r, 302)
         m = re.search('/view/(\w+)', r.location)
         self.assertIsNotNone(m)
         docid = m.group(1)
         r = self.client.get('/raw/' + docid)
-        self.assertEqual(r.status_code, 200)
+        self.assert200(r)
         self.assertEqual(r.content_type, 'application/pdf')
         comm = 'bla bla bla'
         r = self.client.post('/comment/new',
                           data={'docid': docid,
                                 'comment': comm
                                 })
-        self.assertEqual(r.status_code, 302)
+        self.assertStatus(r, 302)
         r = self.client.get(r.location)
-        self.assertEqual(r.status_code, 200)
+        self.assert200(r)
         self.assertIn(comm, r.data)
         r = self.client.get('/view/' + docid)
-        self.assertEqual(r.status_code, 200)
+        self.assert200(r)
         unkore_docid = str(koremutake.decode(docid))
         r = self.client.get('/view/' + unkore_docid)
-        self.assertEqual(r.status_code, 200)
+        self.assert200(r)
 
     def test_no_doc(self):
         r = self.client.get('/view/0')
-        self.assertEqual(r.status_code, 404)
+        self.assert404(r)
         r = self.client.get('/raw/0')
-        self.assertEqual(r.status_code, 404)
+        self.assert404(r)
 
     def test_annotation(self):
         data = { 'doc': 1
@@ -69,7 +69,7 @@ class TestCase(TestCase):
                , 'value': 'Oh oh'
                }
         r = self.client.post('/annotation/new', data=data)
-        self.assertEqual(r.status_code, 200)
+        self.assert200(r)
         d = json.loads(r.data)
         self.assertIn('id', d)
         id_resp = d['id']
@@ -100,7 +100,7 @@ class TestCase(TestCase):
         self.assertEqual(ann['height'], 60)
 
         r = self.client.delete('/annotation/{}'.format(id_retr))
-        self.assertEqual(r.status_code, 200)
+        self.assert200(r)
         r = self.client.get('/view/1/annotations')
         d = json.loads(r.data)
         self.assertNotIn('2', d['data'])
