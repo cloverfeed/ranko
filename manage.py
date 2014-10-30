37f6849bd307ac40ea55971011311563b5c4a725
Etienne Millon <me@emillon.org>
2014-10-30 14:33:42 +0100

    add makeadmin command

--- manage.py
+++ manage.py
@@ -4,7 +4,9 @@ Flask-script stuff
 from flask.ext.migrate import MigrateCommand, stamp
 from flask.ext.script import Manager
 from app.factory import create_app
-from app.models import db
+from app.models import db, User, ROLE_ADMIN
+import string
+import random
 
 def main():
     manager = Manager(create_app)
@@ -16,6 +18,18 @@ def main():
         "Creates database tables from sqlalchemy models"
         db.create_all()
         stamp()
+
+    @manager.command
+    def makeadmin():
+        "Create an admin user with a random password"
+        letters = string.ascii_letters + string.digits
+        length = 20
+        password = ''.join(random.choice(letters) for _ in range(length))
+        user = User('admin', password)
+        user.role = ROLE_ADMIN
+        db.session.add(user)
+        db.session.commit()
+        print password
 
     manager.run()
 
