e24819591e4c36bf74787b78b6c9473ed297aca4
Etienne Millon <me@emillon.org>
2014-10-30 12:05:31 +0100

    Admin panel
    Closes #35

--- app/factory.py
+++ app/factory.py
@@ -2,10 +2,12 @@ from flask import Flask
 import os
 from key import get_secret_key
 from uploads import documents
-from models import db
+import models
 from flask.ext.uploads import configure_uploads
 from flask.ext.assets import Environment, Bundle
 from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqla import ModelView
 
 def create_app(config_file=None):
 
@@ -30,7 +32,7 @@ def create_app(config_file=None):
     if app.config.get('SQLALCHEMY_DATABASE_URI') == '@sql_file':
         here_db = 'sqlite:///' + os.path.join(app.instance_path, 'app.db')
         app.config['SQLALCHEMY_DATABASE_URI'] = here_db
-    db.init_app(app)
+    models.db.init_app(app)
 
     # flask-assets
     assets = Environment(app)
@@ -46,7 +48,21 @@ def create_app(config_file=None):
     assets.register('coffee_app', coffee)
 
     # flask-migrate
-    migrate = Migrate(app, db)
+    migrate = Migrate(app, models.db)
+
+    # flask-admin
+    admin = Admin(app, name=app.name + ' Admin')
+    admin_models = [models.Document,
+                    models.Comment,
+                    models.Annotation,
+                    ]
+
+    class RestrictedModelView(ModelView):
+        def is_accessible(self):
+            return True # FIXME when auth is done
+
+    for model in admin_models:
+        admin.add_view(RestrictedModelView(model, models.db.session))
 
     from views import bp
     app.register_blueprint(bp)
