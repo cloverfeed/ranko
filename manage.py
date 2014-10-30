b43f775a48166531bafa04af81a45b213ca03884
Etienne Millon <me@emillon.org>
2014-10-30 10:03:49 +0100

    generate random docs & comments

--- manage.py
+++ manage.py
@@ -5,8 +5,17 @@ from flask.ext.migrate import MigrateCommand, stamp
 from flask.ext.script import Manager
 from app.factory import create_app
 from app.models import db, User, ROLE_ADMIN
+from mixer.backend.sqlalchemy import Mixer
+from app.models import Document, Comment
+import faker
 import string
 import random
+
+class AppMixer(Mixer):
+    def populate_target(self, values):
+        import ipdb;ipdb.set_trace()
+        target = self.__scheme(**values)
+        return target
 
 def main():
     manager = Manager(create_app)
@@ -30,6 +39,41 @@ def main():
         db.session.add(user)
         db.session.commit()
         print password
+
+    @manager.command
+    def fake():
+        "Populate tables using fake data"
+        fake = faker.Faker()
+
+        def fake_filename():
+            letters = string.ascii_lowercase
+            length = 8
+            extension = '.pdf'
+            base = ''.join(random.choice(letters) for _ in range(length))
+            return base + extension
+
+        def random_id(objs):
+            obj = random.choice(objs)
+            return obj.id
+
+        def generate_document():
+            filename = fake_filename()
+            doc = Document(filename)
+            return doc
+
+        def generate_comment(docs):
+            doc = random_id(docs)
+            assert doc is not None
+            text = fake.text()
+            comm = Comment(doc, text)
+            return comm
+
+        docs = [generate_document() for _ in range(0, 10)]
+        comments = [generate_comment(docs) for _ in range(0, 20)]
+
+        for obj in docs + comments:
+            db.session.add(obj)
+        db.session.commit()
 
     manager.run()
 
