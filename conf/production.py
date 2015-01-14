3de489ff798867c81aa7e2fb5e7a3ac4bf7616cd
Etienne Millon <me@emillon.org>
2015-01-14 17:12:07 +0100

    log exceptions to Slack
    Closes #68

--- conf/production.py
+++ conf/production.py
@@ -1,1 +1,5 @@
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ranko:@/ranko'
+SLACK_API_TOKEN = os.getenv('SLACK_API_TOKEN')
+SLACK_CHANNEL = '#general'
+SLACK_USERNAME = 'Ranko logger'
+SLACK_ICON_URL = 'https://i.imgur.com/QxAyIvW.jpg'
